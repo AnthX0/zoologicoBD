83a66ef7cde515ad952dd0a29f3858c3cb78c0dd
Antonio <95323297+AnthX0@users.noreply.github.com>
2023-05-16 13:59:03 -0700

    Delete Negocio directory

--- Negocio/src/main/java/org/itson/negocio/IQuejaNegocio.java
+++
@@ -1,9 +0,0 @@
-package org.itson.negocio;
-
-public interface IQuejaNegocio {
-
-    public void guardarQueja(String queja, String correoElectronico, String telefono);
-
-    public void validacionQueja(String queja, String correoElectronico, String telefono);
-
-}
