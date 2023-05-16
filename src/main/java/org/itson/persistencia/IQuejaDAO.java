f7376397a9a0cb89080a960233234afb8e3b8865
AnthX0 <victorglezb@gmail.com>
2023-05-16 14:06:01 -0700

    Revertí los cambios anteriores

--- src/main/java/org/itson/persistencia/IQuejaDAO.java
+++ src/main/java/org/itson/persistencia/IQuejaDAO.java
@@ -1,6 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
 package org.itson.persistencia;
 
+/**
+ * @author Victor, Henry, Hermann y Adán
+ * @version IDE 17
+ */
 public interface IQuejaDAO {
-
     public void guardarQueja(String queja, String correoElectronico, String telefono);
 }
