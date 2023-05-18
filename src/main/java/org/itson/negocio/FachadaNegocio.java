908e96e3d180d59b32926d1fd5eae7190908e091
hermanncazares <salo_x360@hotmail.com>
2023-05-17 17:51:11 -0700

    agregue excepciones para llenarlistas

--- src/main/java/org/itson/negocio/FachadaNegocio.java
+++ src/main/java/org/itson/negocio/FachadaNegocio.java
@@ -3,6 +3,7 @@ package org.itson.negocio;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import javax.swing.JOptionPane;
 import org.itson.dominio.Guia;
 import org.itson.dominio.Itinerario;
 import org.itson.dominio.Zona;
@@ -15,18 +16,19 @@ import org.itson.persistencia.DAO.ZonaDAO;
  * @version IDE 17
  */
 public class FachadaNegocio {
+
     // MÉTODOS
-    public void recuperarZonasYGuias(){
+    public void recuperarZonasYGuias() {
         this.recuperarGuias();
         this.recuperarZonas();
     }
-    
-    public void recuperarZonas(){
+
+    public void recuperarZonas() {
         ZonaDAO zonadao = new ZonaDAO();
         zonadao.consultarZonas();
     }
-    
-    public void recuperarGuias(){
+
+    public void recuperarGuias() {
         GuiasDAO guiasdao = new GuiasDAO();
         guiasdao.consultarGuias();
     }
@@ -39,26 +41,36 @@ public class FachadaNegocio {
     public List<String> llenarListaNombreItinerarios() {
         ItinerarioDAO itinerariodao = new ItinerarioDAO();
         List<Itinerario> lista = itinerariodao.consultarItinerarios();
+        if (lista == null || lista.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error al recuperar los itinerarios", "Error", JOptionPane.ERROR_MESSAGE);
+        }
         List<String> listanombres = new ArrayList<>();;
         for (int i = 0; i < lista.size(); i++) {
             listanombres.add(lista.get(i).getNombre());
         }
         return listanombres;
     }
-    
-     public List<Date> llenarListaItinerarioFechaDia() {
+
+    public List<Date> llenarListaItinerarioFechaDia() {
         ItinerarioDAO itinerariodao = new ItinerarioDAO();
         List<Itinerario> lista = itinerariodao.consultarItinerarios();
+        if (lista == null || lista.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error al recuperar los itinerarios", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
         List<Date> listafechadia = new ArrayList<>();;
         for (int i = 0; i < lista.size(); i++) {
             listafechadia.add(lista.get(i).getFechaHoraItinerario());
         }
         return listafechadia;
     }
-     
-      public List<Date> llenarListaItinerarioHora() {
-       ItinerarioDAO itinerariodao = new ItinerarioDAO();
+
+    public List<Date> llenarListaItinerarioHora() {
+        ItinerarioDAO itinerariodao = new ItinerarioDAO();
         List<Itinerario> lista = itinerariodao.consultarItinerarios();
+        if (lista == null || lista.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error al recuperar los itinerarios", "Error", JOptionPane.ERROR_MESSAGE);
+        }
         List<Date> listafechadia = new ArrayList<>();;
         for (int i = 0; i < lista.size(); i++) {
             listafechadia.add(lista.get(i).getFechaHoraItinerario());
