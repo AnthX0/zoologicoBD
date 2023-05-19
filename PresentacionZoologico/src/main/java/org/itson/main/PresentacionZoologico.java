0a8187f2e5f63e002501210a00e49d691d0b7cc0
Antonio <95323297+AnthX0@users.noreply.github.com>
2023-05-18 20:27:50 -0700

    Añadí el proyecto de Presentación

---
+++ PresentacionZoologico/src/main/java/org/itson/main/PresentacionZoologico.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.main;
+
+import org.itson.presentacion.pantallaPrincipal;
+
+/**
+ * @author Victor
+ * @version IDE 17
+ */
+public class PresentacionZoologico {
+    public static void main(String[] args) {
+        pantallaPrincipal principal = new pantallaPrincipal();
+        principal.setVisible(true);
+    }
+}
